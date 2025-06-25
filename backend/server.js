5930119c3959350e8db2f417ceffe76c2fd5f098
Khoa Mai <khoagame2005@gmail.com>
2025-06-25 08:00:14 -0700

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,37 @@
+require('dotenv').config()
+
+
 const express = require('express') 
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workout')
 
 //express app
 const app = express()
 
-//routes
-app.get('/', (req, res) => { 
-    res.json({mssg: 'Welcome to the app'})
+//middleware
+app.use(express.json())
+
+app.use((req,res, next) => { 
+    console.log(req.path, req.method)
+    next()
 })
 
-//listen for requests
-app.listen(4000, () => { 
-    console.log('listening on port 4000!111')
-})
+//routes
+app.use('/api/workouts',workoutRoutes)
+
+//connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => { 
+        //listen for requests
+        app.listen(process.env.PORT, () => { 
+            console.log('connected to db & listening on port', process.env.PORT)
+        })
+
+    })
+    .catch((error) => { 
+        console.log(error)
+    })
+
+
+
+
